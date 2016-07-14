e9f6b596739df82cff8470805e1e7f1f4637edff
minTodroid <tobischu2000@gmail.com>
2016-07-14 19:30:35 +0200

    Update Main.php

--- src/Sounds/Main.php
+++ src/Sounds/Main.php
@@ -27,17 +27,17 @@ class Main extends PluginBase implements Listener {
 		$this->getLogger()->info(C::RED."Deactivated");
 	}
 	public function onJoin(PlayerJoinEvent $event){
-		$pl = $event->getPlayer();
+		$player = $event->getPlayer();
 		$level = $pl->getLevel();
 		$player->getlevel()->addSound(new PopSound($pl));
 	}
 	public function onDeath(PlayerDeathEvent $event){
-		$pl = $event->getPlayer();
+		$player = $event->getPlayer();
 		$level = $pl->getLevel();
 		$player->getlevel()->addSound(new AnvilFallSound($pl));
 	}
 	public function onQuit(PlayerQuitEvent $event){
-		$pl = $event->getPlayer();
+		$player = $event->getPlayer();
 		$level = $pl->getLevel();
 		$player->getlevel()->addSound(new EndermanTeleportSound($pl));
 	}
