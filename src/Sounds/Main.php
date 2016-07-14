bf352de01aa730c1bf359fc0cad1f3ad25fcc1ca
Erkam Kahraman <erkamkahriman@gmail.com>
2016-07-14 19:24:59 +0200

    Add files via upload

---
+++ src/Sounds/Main.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sounds;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\Player;
+use pocketmine\entity\Entity;
+use pocketmine\level\Position;
+use pocketmine\level\sound\EndermanTeleportSound;
+use pocketmine\level\sound\AnvilFallSound;
+use pocketmine\level\sound\Sound;
+use pocketmine\level\sound\PopSound;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener {
+	
+	public function onEnable(){
+		$this->getLogger()->info(C::GREEN."Activated!");
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+	}
+	public function onDisable(){
+		$this->getLogger()->info(C::RED."Deactivated");
+	}
+	public function onJoin(PlayerJoinEvent $event){
+		$pl = $event->getEntity();
+		$level = $pl->getLevel();
+		$level->addSound(new PopSound($pl));
+	}
+	public function onDeath(PlayerDeathEvent $event){
+		$pl = $event->getEntity();
+		$level = $pl->getLevel();
+		$level->addSound(new AnvilFallSound($pl));
+	}
+	public function onQuit(PlayerQuitEvent $event){
+		$pl = $event->getEntity();
+		$level = $pl->getLevel();
+		$level->addSound(new EndermanTeleportSound($pl));
+	}
+}
